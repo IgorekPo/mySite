376accb8b65efe968c68dbd076efcbf00498cbc5
Igor_Polyakov <itworldigpo@gmail.com>
2026-05-04 18:07:06 +0300

    sd

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.addEventListener('load', () => {
   const loader = document.getElementById('loader');
   const content = document.getElementById('content');
 
-  // Ждем завершения анимации (например, 2 секунды, как в CSS)
   setTimeout(() => {
     loader.style.display = 'none';
     content.style.display = 'block';
